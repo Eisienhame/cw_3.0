2a44d264a0464ed7d3af8af178d91eeefc044659
Eisiehame <knurov24@gmail.com>
2023-02-06 22:16:45 +0200

    chutka ostalos'

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+import pytest
+
+from utils.utils import load_operations, ex_operations, last_values, edited_data
+
+def test_load_operations():
+    assert len(load_operations()) > 0
+
+
+def test_ex_operations(list_1, list_2):
+    assert ex_operations([]) == []
+    assert len(ex_operations(list_1)) == 4
+
+def test_last_values():
+    pass
+
+def edited_data():
+    pass
